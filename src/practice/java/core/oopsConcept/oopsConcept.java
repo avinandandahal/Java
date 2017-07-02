a63e1f84c0565d2cd2e0958dd9e68920764e082e
avi <avidahal02@gmail.com>
2017-07-02 17:40:42 -0500

    testing

---
+++ src/practice/java/core/oopsConcept/oopsConcept.java
@@ -0,0 +1,18 @@
+package practice.java.core.oopsConcept;
+
+public class oopsConcept {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Object: Any entity that has state and behavior is known as an object. For example: chair, pen, table, keyboard, bike etc. It can be physical and logical.");
+		System.out.println("Class: Collection of objects is called class. It is a logical entity.");
+		System.out.println("Inheritance: When one object acquires all the properties and behaviours of parent object i.e. known as inheritance. It provides code reusability. It is used to achieve runtime polymorphism.");
+		System.out.println("Polymorphism: When one task is performed by different ways i.e. known as polymorphism. For example: to convince the customer differently, to draw something e.g. shape or rectangle etc.");
+		System.out.println("Abstraction: Hiding internal details and showing functionality is known as abstraction. For example: phone call, we don't know the internal processing.");
+		System.out.println("Encapsulation: Binding (or wrapping) code and data together into a single unit is known as encapsulation. For example: capsule, it is wrapped with different medicines.");		
+		
+		System.out.println("https://www.javatpoint.com/java-oops-concepts");
+
+	}
+
+}
